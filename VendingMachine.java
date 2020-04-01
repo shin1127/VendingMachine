9d06d5571bb251ac0b83a2077e0fbcfe370cc937
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-04-01 12:50:53 +0900

    自販機の動作に関するメソッドを別クラスとして選り分けた

--- VendingMachine.java
+++ VendingMachine.java
@@ -15,6 +15,8 @@ public class VendingMachine {
 		int choiceType = 0;  // 購入時の商品番号の選択
 		Scanner sc = new Scanner(System.in);
 
+		MachineLogic logic = new MachineLogic();
+
 
 		//商品を設定し、リストに追加する
 
@@ -36,7 +38,7 @@ public class VendingMachine {
 
 		// 商品価格の最小値を決定
 
-		minPrice = searchMin(drinklist, minPrice);
+		minPrice = logic.searchMin(drinklist, minPrice);
 
 
 		// ここから自販機の動作
@@ -44,12 +46,12 @@ public class VendingMachine {
 		// お金を投入する
 
 		System.out.println("お金を投入してください");
-		currentMoney = inputNumber(0, 0, sc);
+		currentMoney = logic.inputNumber(0, 0, sc);
 
 
 		// 投入金額が商品の最安値を上回っているか判定
 
-		buyAbility = buyAbility(currentMoney, minPrice);
+		buyAbility = logic.buyAbility(currentMoney, minPrice);
 
 		// 初期値0か、「1.続けて購入」を選択する限り購入動作を続ける
 
@@ -62,20 +64,20 @@ public class VendingMachine {
 
 			// 飲み物のリストを表示する
 
-			displayProduct(drinklist);
+			logic.displayProduct(drinklist);
 
 			// 商品を選択し購入 商品番号以外なら例外処理
 
 			System.out.println("購入したい商品の番号を入力してください");
-			choiceType = inputNumber(1, drinklist.size(), sc);
+			choiceType = logic.inputNumber(1, drinklist.size(), sc);
 
 			// 選択した商品が購入可能か判定し、残高を更新
 
-			currentMoney = buyProduct(currentMoney, drinklist, choiceType - 1);
+			currentMoney = logic.buyProduct(currentMoney, drinklist, choiceType - 1);
 
 			// 続けて購入できるか判定
 
-			buyAbility = buyAbility(currentMoney, minPrice);
+			buyAbility = logic.buyAbility(currentMoney, minPrice);
 
 			if (buyAbility == true) {
 
@@ -83,7 +85,7 @@ public class VendingMachine {
 
 				System.out.println("続けて購入しますか？\n1.続けて購入する 2.お釣りを出す");
 
-				buyContinue = inputNumber(2, 0, sc);
+				buyContinue = logic.inputNumber(2, 0, sc);
 			}
 			// 残金不足のときの表示
 
@@ -101,77 +103,4 @@ public class VendingMachine {
 
 	}
 
-	// 自販機の数字操作メソッド
-	public static int inputNumber(int pattern, int productSize, Scanner sc) {
-		int inputNumber = 0;
-		while(true) {
-			try {
-				sc = new Scanner(System.in);
-				inputNumber = sc.nextInt();
-			}catch (Exception e) {
-				System.out.println("数字以外が入力されました。");
-
-			}
-			// お金を投入する場合
-			if (pattern == 0) {
-				if(inputNumber % 10 == 0  && inputNumber > 0) {
-					break;
-				}
-			}
-			// 商品番号を選択する場合
-			if (pattern == 1) {
-				if (inputNumber > 0 && inputNumber <= productSize) {
-					break;
-				}
-			}
-			// 続けて購入するか選択する場合
-			if (pattern == 2) {
-				if (inputNumber == 1 || inputNumber == 2) {
-					break;
-				}
-			}
-			System.out.print("無効な入力です。\nもう一度入力してください\n>>");
-			sc = new Scanner(System.in);
-		}
-		return inputNumber;
-	}
-
-	// 商品リストを表示する
-	public static void displayProduct(ArrayList<Drink> drinklist) {
-		for(Drink drink : drinklist) {
-			System.out.println(drink.getNumber() + "." + drink.getName()
-					+ drink.getPrice() + "円");
-		}
-	}
-
-	// 商品価格の最小値を求める
-	public static int searchMin(ArrayList<Drink> drinklist, int minPrice) {
-
-		minPrice = drinklist.get(0).getPrice();
-
-		for(Drink drink : drinklist) {
-			minPrice = Math.min(minPrice, drink.getPrice());
-		}
-		return minPrice;
-	}
-
-	// 残金が商品最安値を上回るか判定
-	public static boolean buyAbility(int currentMoney, int minPrice) {
-		boolean buyAbility = currentMoney >= minPrice;
-		return buyAbility;
-	}
-
-	// 選択した番号の商品が購入可能か判定し、残高更新を含む購入処理を行う
-	public static int buyProduct(int currentMoney, ArrayList<Drink> drinklist, int choiceNumber) {
-		int price = drinklist.get(choiceNumber).getPrice();
-		String productName = drinklist.get(choiceNumber).getName();
-		if (currentMoney >= price) {
-			currentMoney -= price;
-			System.out.println(productName + "を購入しました。");
-		}
-		else {
-			System.out.println("残金が足りません。他の商品を選んでください。");
-		}
-		return currentMoney;
-	}
 }
