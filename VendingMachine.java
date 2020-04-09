5f95368c76632cac22afa26ef405e83ed60f80a8
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-04-09 21:58:28 +0900

    Drinkインスタンスの生成をもっと簡単にした　一部の変数を式に書き換えた

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,17 +1,17 @@
 package 自販機;
 
-import static 自販機.isAlcohol.*;
+import static 自販機.IsAlcohol.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class VendingMachine {
-	public static void main(String[] args) {
+    public static <T> void main(final String[] args) {
 
 
-		ArrayList<Drink> drinklist = new ArrayList<>();  // 商品のクラスを入れる
-		int minPrice = 0;  // 商品価格の最小値
-		int currentMoney = 0;  // 自販機内の残金
+        ArrayList<Drink> drinklist = new ArrayList<>();  // 商品のクラスを入れる
+        int minPrice = 0;  // 商品価格の最小値
+	    int currentMoney = 0;  // 自販機内の残金
 		boolean buyAbilityForMoney = false; // 続けて購入可能な残高かを判定
 		int buyContinue = 0;  // 続けて購入するかどうかを判定
 		int choiceType = 0;  // 購入時の商品番号の選択
@@ -23,23 +23,15 @@ public class VendingMachine {
 
 		//商品を設定し、リストに追加する
 
-        Drink coffee = new Drink("コーヒー", 1, 130, SOFTDRINK);
-        drinklist.add(coffee);
+		drinklist.add(new Drink("コーヒー", 130, SOFTDRINK));
+		drinklist.add(new Drink("水",100, SOFTDRINK));
+		drinklist.add(new Drink("コーラ", 160, SOFTDRINK));
+		drinklist.add(new Drink("お茶", 120, SOFTDRINK));
+		drinklist.add(new Drink("レッドブル", 200, SOFTDRINK));
+		drinklist.add(new Drink("ビール", 200, ALCOHOL));
 
-        Drink water = new Drink("水", 2, 100, SOFTDRINK);
-        drinklist.add(water);
 
-        Drink coke = new Drink("コーラ", 3, 160, SOFTDRINK);
-        drinklist.add(coke);
 
-        Drink tea = new Drink("お茶", 4, 120, SOFTDRINK);
-        drinklist.add(tea);
-
-        Drink redbull = new Drink("レッドブル", 5, 200, SOFTDRINK);
-        drinklist.add(redbull);
-
-        Drink beer = new Drink("ビール", 6, 200, ALCOHOL);
-        drinklist.add(beer);
 
 
 		// 商品価格の最小値を決定
@@ -55,16 +47,12 @@ public class VendingMachine {
 		currentMoney = logic.inputNumber(0, 0, sc);
 
 
-		// 投入金額が商品の最安値を上回っているか判定
-
-		buyAbilityForMoney = logic.buyAbility(currentMoney, minPrice);
-
 		// 初期値0か、「1.続けて購入」を選択する限り購入動作を続ける
 
-		while(buyContinue == 0 || buyContinue == 1){
+		while (buyContinue == 0 || buyContinue == 1){
 
 			// 投入金額が商品の最安値を上回ってなければ終了
-			if(buyAbilityForMoney == false) {
+			if (currentMoney - minPrice >= 0 == false) {
 				break;
 			}
 
@@ -79,7 +67,8 @@ public class VendingMachine {
 
 			// 選択した商品が購入可能か年齢から判定する
 
-			buyAbilityForAge = logic.buyAbilityForAge(drinklist, choiceType - 1, sc);
+			buyAbilityForAge =
+					logic.buyAbilityForAge(drinklist, choiceType - 1, sc);
 
 			// 選択した商品に対して購入可能年齢に達していれば購入処理を試みる
 
@@ -94,13 +83,12 @@ public class VendingMachine {
 
 			// 続けて購入できるか判定
 
-			buyAbilityForMoney = logic.buyAbility(currentMoney, minPrice);
-
-			if (buyAbilityForMoney == true) {
+			if (currentMoney - minPrice >= 0 == true) {
 
 				// 続けて購入するか選択 1,2以外なら例外処理
 
-				System.out.println("続けて購入しますか？\n1.続けて購入する 2.お釣りを出す");
+				System.out.println("続けて購入しますか？\n1"
+						+ ".続けて購入する 2.お釣りを出す");
 
 				buyContinue = logic.inputNumber(2, 0, sc);
 			}
@@ -114,7 +102,9 @@ public class VendingMachine {
 
 		// 自販機動作終了時の表示
 
-		System.out.println("お釣り" + currentMoney + "円を返金します。ご利用ありがとうございました。");
+		System.out.println(
+				"お釣り" + currentMoney + "円を返金します。"
+				+ "ご利用ありがとうございました。");
 
 		sc.close();
 
