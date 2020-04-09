f833ba85c45dc00d45f67cbcbb3e47f2222e0856
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-04-10 03:16:55 +0900

    自販機→客へコメントするmachineSayメソッド導入、nextLine()で標準入力を読みきる

--- MachineLogic.java
+++ MachineLogic.java
@@ -1,17 +1,42 @@
 package 自販機;
 
-import static 自販機.isAlcohol.*;
+import static 自販機.IsAlcohol.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MachineLogic {
 
+	public void machineSay(int pattern) {
+
+		String pattern0 = "お金を投入してください";
+		String pattern1 = "購入したい商品の番号を入力してください";
+		String pattern2 = "続けて購入しますか？\n1.続けて購入する 2.お釣りを出す";
+
+		String SayWord;
+
+		switch (pattern) {
+		case 0:
+			SayWord = pattern0;
+			break;
+		case 1:
+			SayWord = pattern1;
+			break;
+		default:
+			SayWord = pattern2;
+		}
+
+		System.out.println(SayWord);
+	}
+
 
 	// 自販機の数字操作メソッド
 	public int inputNumber(int pattern, int productSize, Scanner sc) {
+
 		int inputNumber = 0;
+
 		while(true) {
+
 			try {
 				sc = new Scanner(System.in);
 				inputNumber = sc.nextInt();
@@ -21,7 +46,7 @@ public class MachineLogic {
 			}
 			// お金を投入する場合
 			if (pattern == 0) {
-				if(inputNumber % 10 == 0  && inputNumber > 0) {
+				if (inputNumber % 10 == 0  && inputNumber > 0) {
 					break;
 				}
 			}
@@ -38,8 +63,10 @@ public class MachineLogic {
 				}
 			}
 			System.out.print("無効な入力です。\nもう一度入力してください\n>>");
-			sc = new Scanner(System.in);
+			// sc = new Scanner(System.in);
+			sc.nextLine();  // 入力された内容をすべて読み込む
 		}
+
 		return inputNumber;
 	}
 
@@ -56,27 +83,21 @@ public class MachineLogic {
 
 		minPrice = drinklist.get(0).getPrice();
 
-		for(Drink drink : drinklist) {
+		for (Drink drink : drinklist) {
 			minPrice = Math.min(minPrice, drink.getPrice());
 		}
 		return minPrice;
 	}
 
-	// 残金が商品最安値を上回るか判定
-	public boolean buyAbility(int currentMoney, int minPrice) {
-		boolean buyAbility = currentMoney >= minPrice;
-		return buyAbility;
-	}
-
 	// 選択した番号の商品が購入可能か判定し、残高更新を含む購入処理を行う
 	public int buyProduct(int currentMoney, ArrayList<Drink> drinklist, int choiceNumber) {
+
 		int price = drinklist.get(choiceNumber).getPrice();
 		String productName = drinklist.get(choiceNumber).getName();
 		if (currentMoney >= price) {
 			currentMoney -= price;
 			System.out.println(productName + "を購入しました。");
-		}
-		else {
+		}else {
 			System.out.println("残金が足りません。他の商品を選んでください。");
 		}
 		return currentMoney;
@@ -86,23 +107,24 @@ public class MachineLogic {
 	// アルコール飲料が選択された時、年齢確認を行い20歳未満なら購入処理をスキップする
 	public boolean buyAbilityForAge(ArrayList<Drink> drinklist, int choiceType, Scanner sc) {
 		int inputNumber = 0;
-		isAlcohol isAlcohol = drinklist.get(choiceType).getIsAlcohol();
+		IsAlcohol isAlcohol = drinklist.get(choiceType).getIsAlcohol();
 		if (isAlcohol == ALCOHOL) {
 			System.out.println(
 					"選択した商品はアルコール飲料です。\n20歳以上ですか？"
 					+ "1.はい 2.いいえ");
 
-			while(true) {
+			while (true) {
 				try {
 					sc = new Scanner(System.in);
 					inputNumber = sc.nextInt();
-				}catch(Exception e) {
+				}catch (Exception e) {
 				System.out.println("数字以外が入力されました。");
 				}
 				if (inputNumber == 1 || inputNumber == 2) {
 					break;
 				}
-				System.out.println("無効な入力です。\nもう一度入力してください\n>>");
+				System.out.println("無効な入力です。"
+						+ "\nもう一度入力してください>> ");
 				sc = new Scanner(System.in);
 
 			}
